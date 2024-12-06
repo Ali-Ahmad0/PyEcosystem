30110f3b72a1802c4410023517a4d6a6b53c90b6
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-12-06 21:36:42 +0500

    basic tilemap loading

--- test.py
+++ test.py
@@ -1,33 +1,29 @@
 import pygame
 from tilemap import *
-from grid import *
+
 
 def main():
     pygame.init()
     pygame.display.set_caption("Test")
     
-    # Set grid size and cell size
-    grid_size = 32
-    cell_size = 16  
-    
-    # Calculate window size
-    window_size = grid_size * cell_size
-    
     # Create screen
-    screen = pygame.display.set_mode((window_size, window_size))
+    screen = pygame.display.set_mode((480, 360))
     
     # grid = Grid(screen, grid_size, cell_size)
 
     # Load tilemap
-    tilemap_object = Tilemap("assets/tilemap/spritesheet.png", "assets/tilemap/tilemap.json")
+    tilemap_object = Tilemap("assets/tilemap/isometric_tileset.png", "assets/tilemap/tilemap.json")
     
     while True:
-        for i in range(11):
-            for j in range(11):
-                index = i + 11 * j
-                if index < len(tilemap_object.tiles):
-                    screen.blit(tilemap_object.tiles[index].convert(), (i * 32, j * 32))
-        
+        # Test tileset loading
+        # for i in range(11):
+        #     for j in range(11):
+        #         index = i + 11 * j
+        #         if index < len(tilemap_object.tiles):
+        #             screen.blit(tilemap_object.tiles[index].convert(), (i * 32, j * 32))
+
+        tilemap_object.render(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
--- tilemap.py
+++ tilemap.py
@@ -1,33 +1,78 @@
 
 import pygame
+import json
 
-class Tilemap():
-    def __init__(self, tileset_path, tilemap_path):
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, position, surface, *groups):
+        super().__init__(*groups)
+        self.image = surface
+        self.rect = self.image.get_rect(topleft=position)
+
+class Tileset():
+    def __init__(self, tileset_path):
         self.tileset_path = tileset_path
-        self.tilemap_path = tilemap_path
 
-        self.tileset = pygame.image.load(tileset_path)
+        self.image = pygame.image.load(tileset_path)
         self.tiles = []
 
         self.load_tiles()
 
-        self.tilemap = None
-
     # Load individual tiles
     def load_tiles(self):
         # Tileset dimensions
-        w, h = self.tileset.get_width(), self.tileset.get_height()
+        w, h = self.image.get_width(), self.image.get_height()
 
         # Tile size
         dx = 32
-        tile_height = 32
+        dy = 32
 
         # Iterate over the tileset to extract each tile
-        for y in range(0, h, tile_height):
+        for y in range(0, h, dy):
             for x in range(0, w, dx):
-                # Create a tile
-                tile = pygame.Surface((dx, tile_height), pygame.SRCALPHA)  
-                tile.blit(self.tileset, (0, 0), pygame.Rect(x, y, dx, tile_height))
+                # Split an individual tile surface from the tileset
+                tile_surface = pygame.Surface((dx, dy), pygame.SRCALPHA)  
+                tile_surface.blit(self.image, (0, 0), pygame.Rect(x, y, dx, dy))
 
-                # Add the tile surface to the list
-                self.tiles.append(tile)
+                # Add the tile to the list
+                self.tiles.append(tile_surface)
+
+class Tilemap():
+    def __init__(self, tileset_path, tilemap_path):
+        self.tileset = Tileset(tileset_path)
+
+        self.tilemap_path = tilemap_path
+        self.sprite_group = pygame.sprite.Group()
+
+        self.tilemap = None
+        self.map_w = 0
+        self.map_h = 0
+
+        self.map_data = []
+        self.load_tilemap()
+
+    # Draw the tilemap based on tilemap data
+    def load_tilemap(self):
+        with open(self.tilemap_path, "r") as file:
+            map_data = json.load(file)
+
+        # Get array of tile index
+        self.map_data = map_data['layers'][0]['data']
+        
+        # Get height and width (in no. of tiles)
+        self.map_w = map_data['width']
+        self.map_h = map_data['height']
+
+        for row in range(self.map_h):
+            for col in range(self.map_w):
+                # Calculate the tile index
+                tile_index = self.map_data[col + self.map_w * row] - 1
+
+                # Skip if (empty tile)
+                if tile_index >= 0:  
+                    # Generate a new tile
+                    tile_surface = self.tileset.tiles[tile_index]
+                    tile = Tile((col * 32, row * 32), tile_surface, self.sprite_group)
+
+    def render(self, screen):
+        for tile in self.sprite_group:
+            screen.blit(tile.image, tile.rect)
