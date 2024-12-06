60831c8f47099e0ba4dd4544aa564063ba5891f2
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-12-06 21:37:43 +0500

    minor changes

--- test.py
+++ test.py
@@ -9,19 +9,10 @@ def main():
     # Create screen
     screen = pygame.display.set_mode((480, 360))
     
-    # grid = Grid(screen, grid_size, cell_size)
-
     # Load tilemap
     tilemap_object = Tilemap("assets/tilemap/isometric_tileset.png", "assets/tilemap/tilemap.json")
     
     while True:
-        # Test tileset loading
-        # for i in range(11):
-        #     for j in range(11):
-        #         index = i + 11 * j
-        #         if index < len(tilemap_object.tiles):
-        #             screen.blit(tilemap_object.tiles[index].convert(), (i * 32, j * 32))
-
         tilemap_object.render(screen)
 
         for event in pygame.event.get():
