7955b1056603e2f7f8b5cd435c092fcc49392ce2
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-12-09 19:58:11 +0500

    fixed mistake

--- Grid.py
+++ Grid.py
@@ -1,19 +1,7 @@
-import pygame
-import math
 
 class Grid:
-    def __init__(self, screen, map_data):
+    def __init__(self, screen, tilemap):
         self.screen = screen
         self.cell_size = 32
-        self.grid_size = int(math.sqrt(len(map_data)))
-        self.window_size = self.grid_size * self.cell_size
+        self.grid_size = tilemap.map_w * tilemap.map_h
         
-        # Initialize grid based on map_data
-        self.grid = [[-1 if map_data[row * self.grid_size + col] == -1 else 0 
-                      for col in range(self.grid_size)] 
-                     for row in range(self.grid_size)]
-        
-        
-    
-        
-        pygame.display.flip()
