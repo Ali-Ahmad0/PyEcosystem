3d7047f7394706fd364997f4f7b46f7fea9b2521
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-12-12 11:50:05 +0500

    multi layered tilemap implemented

--- tilemap.py
+++ tilemap.py
@@ -58,28 +58,34 @@ class Tilemap():
         with open(self.tilemap_path, "r") as file:
             map_data = json.load(file)
 
-        # Get array of tile index
-        self.map_data = map_data['layers'][0]['data']
+        # Get tilemap data for each layer
+        layers = map_data['layers']
+
+        for _ in range(len(layers)):
+            self.map_data.append(list)
         
         # Get height and width (in no. of tiles)
         self.map_w = map_data['width']
         self.map_h = map_data['height']
 
-        for row in range(self.map_h):
-            for col in range(self.map_w):
-                # Calculate the tile index
-                tile_index = self.map_data[col + self.map_w * row] - 1
+        for i, layer in enumerate(layers):
+            for row in range(self.map_h):
+                self.map_data[i] = layer['data']
 
-                # Skip if (empty tile)
-                if tile_index >= 0:  
-                    # Get the surface of the tile
-                    tile_surface = self.tileset.tiles[tile_index]
+                for col in range(self.map_w):
+                    # Calculate the tile index
+                    tile_index = self.map_data[i][col + self.map_w * row] - 1
 
-                    # Calculate x, y coordinates
-                    x = self.position[0] + (col - row) * 16
-                    y = self.position[1] + (col + row) * 8
+                    # Skip if (empty tile)
+                    if tile_index >= 0:  
+                        # Get the surface of the tile
+                        tile_surface = self.tileset.tiles[tile_index]
 
-                    tile = Tile((x, y), tile_surface, self.sprite_group)
+                        # Calculate x, y coordinates
+                        x = self.position[0] + (col - row) * 16
+                        y = self.position[1] + (col + row) * 8
+
+                        tile = Tile((x, y), tile_surface, self.sprite_group)
 
     def render(self, screen):
         for tile in self.sprite_group:
