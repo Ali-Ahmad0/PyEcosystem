457b227e98b03570d0353e647d1f245a202927d7
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-12-13 19:54:40 +0500

    grid generation done

--- grid.py
+++ grid.py
@@ -14,6 +14,18 @@ class Grid:
             print(col)
 
     def generate_grid(self, map_data):
+        
+        # Check if a tile is next to water
+        def is_next_to_water(x, y, grid_size):
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Left, Right, Up, Down
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < grid_size[0] and 0 <= ny < grid_size[1]:
+                    if self.data[ny][nx] == 'W':
+                        return True
+                    
+            return False
+
         # Initialize the world grid
         for i in range(len(map_data)):
             if (i % self.grid_size[0] == 0):
@@ -26,10 +38,11 @@ class Grid:
             # 29 to 36 - Grass Tiles
             # 110 to 121 - Water Tiles
 
-            # 'X' - invalid / water
+            # 'X' - invalid
+            # 'W' - water
             # ' ' - path
             # 'G' - grass
-            # 'W' - next to water
+            # 'N' - next to water
             tile_type = ''
             if (tile_index == 0):
                 tile_type = 'X'
@@ -38,9 +51,15 @@ class Grid:
                 tile_type = 'G'
             
             elif (tile_index >= 111 and tile_index <= 122):
-                tile_type = 'X'
+                tile_type = 'W'
 
             else:
                 tile_type = ' '
             
             self.data[j].append(tile_type)
+
+        # Update tiles next to water
+        for y in range(self.grid_size[1]):
+            for x in range(self.grid_size[0]):
+                if self.data[y][x] == ' ' and is_next_to_water(x, y, self.grid_size):
+                    self.data[y][x] = 'N'
